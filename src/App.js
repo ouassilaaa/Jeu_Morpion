38f23f0e31400f7a34e1701759b447014e813382
ouassilaaa <ouassidu31@hotmail.fr>
2023-11-08 15:21:29 +0100

    jeu morpion fonctionnel

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Morpion from './Components/Morpion/Morpion';
+
+function App() {
+  return (
+    
+    <div>
+      <Morpion/>
+    </div>
+  );
+}
+
+export default App;
